fbf9c766c0d9559f6c18d566c4f6aaf0e2689816
Luiz Casagrande <casagrandehluiz@gmail.com>
2025-08-21 20:04:47 -0300

    scroll top function

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import {Component} from '@angular/core';
 import {PrimeNG} from 'primeng/config';
 import {Header} from './header/header';
 import {RouterOutlet} from '@angular/router';
+import {ScrollTop} from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-root',
   imports: [
     Header,
     RouterOutlet,
+    ScrollTop,
   ],
   templateUrl: './app.html',
 })
